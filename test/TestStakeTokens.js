cb20a67463a03269ab39e60d69a0182f31f9af7f
CodeStorm <31198108+PrajwalMore@users.noreply.github.com>
2021-07-23 22:52:33 +0530

    Writing Wei to Eth
    converted wei to eth using web3 for better readability.📖

--- test/TestStakeTokens.js
+++ test/TestStakeTokens.js
@@ -1,6 +1,5 @@
 const stakeTokens = artifacts.require("./stakeTokens.sol");
 
-//const BN = web3.utils.BN;
 const chai = require("./setupchai.js");
 let catchRevert = require("./exceptions.js").catchRevert;
 const truffleAssert = require('truffle-assertions');
@@ -20,8 +19,8 @@ contract("stakeTokens", accounts => {
     const [deployerAccount, staker, userThird] = accounts;
 
     beforeEach(async () => {
-        instance = await stakeTokens.new("1000000000000000000000");
-        instance.transfer(instance.address, "100000000000000000000", { from: deployerAccount });//transferring to stakeTokens contract
+        instance = await stakeTokens.new(web3.utils.toWei("1000", "ether"));
+        instance.transfer(instance.address, web3.utils.toWei("100", "ether"), { from: deployerAccount });//transferring to stakeTokens contract
     });
 
 
@@ -29,23 +28,21 @@ contract("stakeTokens", accounts => {
         return expect(instance.balanceOf(instance.address)).to.not.equal(0);
     });
     it("User can stake tokens.", async () => {
-        //this.web3=await getWeb3();
-        //let stakeAmount = this.web3.toWei("10","ether");
-        let stakeAmount = "10000000000000000000";
-        await instance.transfer(staker, "100000000000000000000", { from: deployerAccount });
+        let stakeAmount = web3.utils.toWei("10", "ether");
+        await instance.transfer(staker, web3.utils.toWei("100", "ether"), { from: deployerAccount });
         await instance.stakeToken(stakeAmount, { from: staker });
         let holdsStake = await instance.stakeOf(staker);
         return expect(holdsStake.toString()).to.equal(stakeAmount.toString());
     });
     it("Deployer/ owner can send tokens to other addresses.", async () => {
         let stakerBalanceBefore = instance.balanceOf(staker);
-        await instance.transfer(staker, 10, { from: deployerAccount });
+        await instance.transfer(staker, web3.utils.toWei("10", "ether"), { from: deployerAccount });
         let stakerBalanceAfter = instance.balanceOf(staker);
         return expect(stakerBalanceBefore).to.not.equal(stakerBalanceAfter);
     });
     it("User can withdraw reward.", async () => {
-        let stakeAmount = "10000000000000000000";
-        await instance.transfer(staker, "100000000000000000000", { from: deployerAccount });
+        let stakeAmount = web3.utils.toWei("10", "ether");
+        await instance.transfer(staker, web3.utils.toWei("100", "ether"), { from: deployerAccount });
         let stakerBalanceBefore = await instance.balanceOf(staker);
         await instance.stakeToken(stakeAmount, { from: staker });
 
@@ -55,8 +52,8 @@ contract("stakeTokens", accounts => {
         return expect(stakerBalanceBefore.toString()).to.equal(stakerBalanceAfter.toString());//assuming that staker is withdrawing before 5 minutes in this case.
     });
     // it("After withdrawing reward interest gets added to accounts.", async () => {
-    //     let stakeAmount = "10000000000000000000";
-    //     await instance.transfer(staker, "100000000000000000000", { from: deployerAccount });
+    //     let stakeAmount = web3.utils.toWei("10","ether");
+    //     await instance.transfer(staker, web3.utils.toWei("100","ether"), { from: deployerAccount });
     //     let stakerBalanceBefore = await instance.balanceOf(staker);
     //     await instance.stakeToken(stakeAmount, { from: staker });
     //     //console.log( +stakerBalanceBefore + 200000000000000000);
@@ -74,8 +71,8 @@ contract("stakeTokens", accounts => {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
     it("User can withdraw reward and after withdrawing reward staker's reward becomes 0", async () => {
-        let stakeAmount = "10000000000000000000";
-        await instance.transfer(staker, "100000000000000000000", { from: deployerAccount });
+        let stakeAmount = web3.utils.toWei("10", "ether");
+        await instance.transfer(staker, web3.utils.toWei("100", "ether"), { from: deployerAccount });
         await instance.stakeToken(stakeAmount, { from: staker });
         await instance.withdraw({ from: staker });
         let stakeOfstaker = await instance.stakeOf(staker);
@@ -83,22 +80,21 @@ contract("stakeTokens", accounts => {
     });
 
     it("User can check time after token staked.", async () => {
-        let stakeAmount = "10000000000000000000";
-        await instance.transfer(staker, "100000000000000000000", { from: deployerAccount });
+        let stakeAmount = web3.utils.toWei("10", "ether");
+        await instance.transfer(staker, web3.utils.toWei("100", "ether"), { from: deployerAccount });
         await instance.stakeToken(stakeAmount, { from: staker });
         return expect(await instance.timeAfterStaked({ from: staker })).to.not.equal(0);
-        
+
     });
 
     it("should revert if user tries to call timeAfterStaked() without staking", async () => {
-           await truffleAssert.reverts(instance.timeAfterStaked({ from: userThird }));
-        
+        await truffleAssert.reverts(instance.timeAfterStaked({ from: userThird }));
     });
 
     it("It's not possible withdraw if caller haven't staked.", async () => {
-      
-            await truffleAssert.reverts(instance.withdraw({ from: userThird }));
-        
+
+        await truffleAssert.reverts(instance.withdraw({ from: userThird }));
+
     });
 
 });
